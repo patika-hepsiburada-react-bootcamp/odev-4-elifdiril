be37dbce6dd49699befcb952801e154b77fa9571
elif <elifdiril6@gmail.com>
2021-10-17 18:42:49 +0300

    unnecessary import removed

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, Row, Col } from 'reactstrap';
+import { Card, CardText, CardBody, Row, Col } from 'reactstrap';
 import '../App.css';
 import Humid from './LottieComponents/Humid';
 import WeatherAnimation from './LottieComponents/WeatherAnimation';
